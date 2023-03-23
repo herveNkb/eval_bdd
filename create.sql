aa16acbdbcefa9c76d6651eefc6356a6ba59ee60
AirV <herve.jacotet@sfr.fr>
2023-03-23 22:07:23 +0100

    feat(data) Fictitious data Add fictitious data into the tables.

--- create.sql
+++ create.sql
@@ -184,8 +184,71 @@ CREATE TABLE IF NOT EXISTS price_list_payment
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
 
--- Insertion des données dans les tables
+-- Insertion des données fictives dans les tables
+-- Table user
+INSERT INTO user (id, email, password, role, first_name, last_name) VALUES
+       (1, 'gbraganza0@freewebs.com', '$2y$10$ZzPYmLAS/.pbjx7kidD25OEIkCmB62rVdRvD4vAV7oL.IHv4L.eeC', 'ROLE_ADMIN', 'Sélène','Braganza'),
+       (2, 'lmoir1@reddit.com', '$2y$10$rOPltkBiTNgqtsKNP9aFFOX1t4avYF.UYwTofzV7zKSHSer/wlWAy', 'ROLE_USER', 'Maëlle', 'Moir'),
+       (3, 'nparrin2@irs.gov', '$2y$10$JtH/4.TN89mOzES.Sp42i.WUGx3PVOf4f63iXOF1Rj/e5ObHcemUq', 'ROLE_USER', 'Andréanne','Parrin'),
+       (4, 'dcone3@arstechnica.com', '$2y$10$YVUV468TwpAdZ9HHKdBLpOzaGdVQCYqiALVedn5xsTtdh3s7fhQOy', 'ROLE_USER', 'Garçon','Cone'),
+       (5, 'arosone4@printfriendly.com', '$2y$10$6R8wIFCg7pYy.fZzYExFqeqXTUlgqu.7zLOXdRWCPOwFTl02x7lSa', 'ROLE_USER', 'Athéna','Rosone');
+-- Table cinema_complex
+INSERT INTO cinema_complex (id, name, address, zip_code, city, phone_number) VALUES
+        (1, 'Totoro cinema', '30 rue du trou noir', '70313', 'Ainvelle', '0485326908'),
+        (2, 'Totoro complex', '69 ave Jean Jaures', '67002', 'Barr', '05376490'),
+        (3, 'Totoro écran blanc', '20 Village Green Place', '25236', 'Anteuil', '0390768225'),
+        (4, 'Totoro 7eme art', '247 rue du ciel', '30758', 'Alès', '0412549635');
 
+-- Table cinema_rooms
+INSERT INTO cinema_rooms (hall_number, number_of_places, id) SELECT 1, 50, id FROM cinema_complex WHERE name = 'Totoro cinema';
+TRUNCATE TABLE cinema_rooms;
+INSERT INTO cinema_rooms (hall_number, number_of_places, id)
+VALUES (1, 50, 2), (2, 70, 2), (3, 100, 2);
+
+-- Table film_show
+INSERT INTO film_show (id, date) VALUES
+        (1, '2023-02-02 06:23:20'),
+        (2, '2022-12-15 23:41:47'),
+        (3, '2023-01-06 13:12:00'),
+        (4, '2023-02-04 01:22:16'),
+        (5, '2022-08-30 22:26:38'),
+        (6, '2022-06-26 07:20:27'),
+        (7, '2022-08-05 03:12:35'),
+        (8, '2022-07-10 15:08:55'),
+        (9, '2023-01-24 17:10:21'),
+        (10, '2022-12-16 11:36:59'),
+        (11, '2022-11-06 08:24:49'),
+        (12, '2022-10-18 05:24:22'),
+        (13, '2022-11-25 02:34:13'),
+        (14, '2022-05-17 11:32:15'),
+        (15, '2022-07-02 12:41:46'),
+        (16, '2022-05-07 03:15:46'),
+        (17, '2022-10-09 10:54:07'),
+        (18, '2022-06-15 17:58:38'),
+        (19, '2022-07-17 07:34:53'),
+        (20, '2022-12-24 00:49:23');
+-- Table movie
+INSERT INTO movie (id, title, synopsis, movie_type, duration, director) VALUES
+        (1, 'Hounds, The', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Crime|Horror|Thriller', '1h30', 'Franchioni'),
+        (2, 'Bad Company', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Action|Crime|Drama', '2h45', 'Blasi'),
+        (3, 'Jesse Stone: Innocents Lost', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Crime|Drama', '2h47', 'Creaser'),
+        (4, 'Valachi Papers,The', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Crime|Drama', '1h54', 'Klamman'),
+        (5, 'Fingers at the Window', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.U', 'Drama|Film-Noir|Mystery', '2h33', 'Dunlap'),
+        (6, 'Lion King II: Simba''s Pride, The', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Adventure|Animation|Children|Musical|Romance', '1h48', 'Give'),
+        (7, 'Perfect Host, The', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Crime|Drama|Thriller', '1h36', 'Kerwin'),
+        (8, 'Paperboy, The', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Thriller', '2h54', 'Blood'),
+        (9, 'Aurora', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Romance|Sci-Fi', '2h44', 'Giabucci'),
+        (10, 'Flower Drum Song', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Comedy|Musical|Romance', '2h18', 'Peever'),
+        (11, 'Winning of Barbara Worth, The', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Drama|Romance|Western', '3h52', 'Lenthall'),
+        (12, 'Spring Subway', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Drama|Romance', '1h55', 'Brower'),
+        (13, 'They Came to Cordura', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Western', '1h56', 'Riccetti'),
+        (14, 'Misunderstood', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Drama', '2h34', 'Dobie'),
+        (15, 'Bekas', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Drama', '3h21', 'Sleite'),
+        (16, 'Architect, The', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Drama', '2h15', 'Pampling'),
+        (17, 'Shit Year', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Drama', '3h54', 'Janowicz'),
+        (18, 'Brasher Doubloon, The', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Crime|Drama|Film-Noir|Mystery', '2h23', 'Najera'),
+        (19, 'Failan', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Drama|Romance', '2h14', 'Mills'),
+        (20, 'Sentimental Swordsman, The (To ching chien ko wu ching chien)', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Nisi lacus sed viverra tellus in hac habitasse. Consequat interdum varius sit amet mattis vulputate enim nulla. Consectetur purus ut faucibus pulvinar.', 'Action|Adventure', '2h41', 'Humburton');
 
 
 -- Table customer
